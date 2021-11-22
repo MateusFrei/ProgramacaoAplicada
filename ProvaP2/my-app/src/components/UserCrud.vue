02fde6c0cc2f741e31f1eda40d88832b9bbbc403
MateusFrei <41398101+MateusFrei@users.noreply.github.com>
2021-11-22 10:27:44 -0300

    up

--- ProvaP2/my-app/src/components/UserCrud.vue
+++ ProvaP2/my-app/src/components/UserCrud.vue
@@ -10,45 +10,60 @@
         <v-toolbar-title>My CRUD</v-toolbar-title>
           <v-divider class="mx-4" inset vertical></v-divider>
             <v-dialog v-model="dialog" max-width="700px">
-                <template v-slot:activator="{on, attrs }">
-                  <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">Novo Item</v-btn>
-                </template>
+              <template v-slot:activator="{on, attrs }">
+                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">Novo Item</v-btn>
+              </template>
 
-                <v-card>
-                  <v-card-title>
-                    <span class="headline">{{ formTitle }}</span>
-                  </v-card-title>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
 
-                  <v-card-text>
-                    <v-container>
-                      <v-row>
-                        <v-col cols="12" sm="2" md="2">
-                          <v-text-field
-                            v-model="editedItem.id"
-                            label="Id"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field
-                            v-model="editedItem.name"
-                            label="Nome da Bike"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                          <v-text-field
-                            v-model="editedItem.location"
-                            label="Localização"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="2" md="2">
-                          <v-text-field
-                            v-model="editedItem.diaria"
-                            label="Diaria"
-                          ></v-text-field>
-                        </v-col>
-                      </v-row>
-                    </v-container>
-                  </v-card-text>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+
+                    <v-col cols="12" sm="2" md="2">
+                      <v-text-field
+                        v-model="editedItem.id"
+                        label="Id"
+                      ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="Nome da Bike"
+                      ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.location"
+                        label="Localização"
+                      ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12" sm="2" md="2">
+                      <v-text-field
+                        v-model="editedItem.diaria"
+                        label="Diaria"
+                      ></v-text-field>
+                    </v-col>
+
+                  <v-col cols="12" sm="6" md="4">
+                    <v-select
+                      v-model="editedItem.modelo_id"
+                      :items="modelos"
+                      label="Modelo"
+                      item-text="descricao"
+                      item-value="id"
+                      return-value
+                      ></v-select>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
                   
                     <v-card-actions>
                       <v-spacer></v-spacer>
@@ -110,8 +125,9 @@ export default {
   }),
   methods: {
     descricao(id){
-      var descricao = this.tipos.find((x) => x.id ===id);
-      descricao = descricao ? descricao.descricao : "Tipos Desconhecidos";
+      var descricao = this.modelos.find((x) => x.id ===id);
+      descricao = descricao ? descricao.descricao : "Modelo Desconhecido";
+      return descricao;
     },
 
     inicializa() {
